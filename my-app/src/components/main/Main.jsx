6ee7910fd086c5093ec2f8dbc6a9742797925615
tanu2110 <tanushree.tnay@gmail.com>
2023-04-02 14:34:08 +0530

    final commit

--- my-app/src/components/main/Main.jsx
+++ my-app/src/components/main/Main.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import reactjs from "../../reactjs.svg";
 import "./main.css";
 
-export default function Header(){
+export default function Main(){
     return (
         <div className="body">
-
+            <div className="title">
+                <h2>Fun facts about React</h2>
+            </div>
+            <div className="points">
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
+            <div className="log">
+                <img src={reactjs} alt=" " />
+            </div>
+            
         </div>
 
     );
